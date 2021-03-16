ed5367a23d25bf39dde1142631bda45fe6fac745
KimiaAsarroodi <asarroodikim@yahoo.com>
2021-03-16 15:31:18 +1030

    reading all the entries and writting them to the table

--- index.js
+++ index.js
@@ -1,45 +1,58 @@
 function getJsonObject(path, success, error) {
   var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-          if (xhr.status === 200) {
-              if (success) success(JSON.parse(xhr.responseText));
-          } else {
-              if (error) error(xhr);
-          }
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        if (success) success(JSON.parse(xhr.responseText));
+      } else {
+        if (error) error(xhr);
       }
+    }
   };
   xhr.open("GET", path, true);
   xhr.send();
 }
 
-window.onload = function (){
+function writeTable(list) {
+  for (var i = 0; i < list.length; i++) {
+    console.log(list[i]);
+    var tbodyRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+    var newRow = tbodyRef.insertRow();
+    var newTitle = newRow.insertCell();
+    var newAuthors = newRow.insertCell();
+    var newYear = newRow.insertCell();
+    var newPrice = newRow.insertCell();
+    var newPublisher = newRow.insertCell();
+    var newCategory = newRow.insertCell();
+    newTitle.innerHTML = bookList[i].title;
+    newAuthors.innerHTML = bookList[i].authors;
+    newYear.innerHTML = bookList[i].year;
+    newPrice.innerHTML = bookList[i].price;
+    newPublisher.innerHTML = bookList[i].publisher;
+    newCategory.innerHTML = bookList[i].category;
+    //var newTitle = document.createTextNode(bookList[0].title);
+    //var newAuthors = document.createTextNode(bookList[0].authors);
+    // newCell.appendChild(newTitle);
+  }
+
+
+}
+
+window.onload = function () {
   bookList = []; // book list container
   getJsonObject('data.json',
-    function(data) {
-        bookList = data; // store the book list into bookList
-        console.log(bookList); // print it into console (developer tools)
-        console.log(bookList[0].authors); // print the first book object into console 
-        // here you can call methods to laod or refresh the page 
-        // loadBooks() or refreshPage()
-        var tbodyRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
-        var newRow = tbodyRef.insertRow();
-        var newTitle= newRow.insertCell(0);
-        var newAuthors = newRow.insertCell(1);
-        var newYear = newRow.insertCell(1);
-        var newPrice = newRow.insertCell(1);
-        var newPublisher = newRow.insertCell(1);
-        var newCategory= newRow.insertCell(1);
-        newTitle.innerHTML = bookList[0].title;
-        newAuthors.innerHTML = bookList[0].authors;
-        newYear.innerHTML = bookList[0].year;
-        newPrice.innerHTML = bookList[0].price;
-        newPublisher.innerHTML = bookList[0].publisher;
-        newCategory.innerHTML = bookList[0].category;
-        //var newTitle = document.createTextNode(bookList[0].title);
-        //var newAuthors = document.createTextNode(bookList[0].authors);
-       // newCell.appendChild(newTitle);
-        },
-    function(xhr) { console.error(xhr);
-     }
-); }
+    function (data) {
+      bookList = data; // store the book list into bookList
+      // console.log(bookList); // print it into console (developer tools)
+      // console.log(bookList[0].authors); // print the first book object into console 
+      // here you can call methods to laod or refresh the page 
+      // loadBooks() or refreshPage()
+      writeTable(bookList);
+
+
+    },
+    function (xhr) {
+      console.error(xhr);
+    }
+  );
+}
