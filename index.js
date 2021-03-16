bc36bc01828e0a396a8c0f3d252b90c900fea2bb
KimiaAsarroodi <asarroodikim@yahoo.com>
2021-03-16 15:04:54 +1030

    writting first row's data into table

--- index.js
+++ index.js
@@ -19,12 +19,27 @@ window.onload = function (){
     function(data) {
         bookList = data; // store the book list into bookList
         console.log(bookList); // print it into console (developer tools)
-        console.log(bookList[0]); // print the first book object into console 
+        console.log(bookList[0].authors); // print the first book object into console 
         // here you can call methods to laod or refresh the page 
         // loadBooks() or refreshPage()
-    },
-    function(xhr) { console.error(xhr); }
-);
-
-
-}
+        var tbodyRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+        var newRow = tbodyRef.insertRow();
+        var newTitle= newRow.insertCell(0);
+        var newAuthors = newRow.insertCell(1);
+        var newYear = newRow.insertCell(1);
+        var newPrice = newRow.insertCell(1);
+        var newPublisher = newRow.insertCell(1);
+        var newCategory= newRow.insertCell(1);
+        newTitle.innerHTML = bookList[0].title;
+        newAuthors.innerHTML = bookList[0].authors;
+        newYear.innerHTML = bookList[0].year;
+        newPrice.innerHTML = bookList[0].price;
+        newPublisher.innerHTML = bookList[0].publisher;
+        newCategory.innerHTML = bookList[0].category;
+        //var newTitle = document.createTextNode(bookList[0].title);
+        //var newAuthors = document.createTextNode(bookList[0].authors);
+       // newCell.appendChild(newTitle);
+        },
+    function(xhr) { console.error(xhr);
+     }
+); }
