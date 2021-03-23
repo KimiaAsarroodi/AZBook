eb8c58110e9b38ed3ed921c908985b19e3d882cd
KimiaAsarroodi <asarroodikim@yahoo.com>
2021-03-23 16:02:23 +1030

    final version

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-//var searched = [];
-//var categorized = [];
-//var result = [];
-var selectedId = "";
-var countbooks = 0;
-var bookList=[];
-var count =0;
+var bookList = [];
 var currentTable = [];
-var searched = [];
-var counter = 0;
+var countSearched = 0;
+var countCategorized = 0;
+var countBooks = 0;
+var selectedId = "";
 
 function getJsonObject(path, success, error) {
   var xhr = new XMLHttpRequest();
@@ -23,10 +19,12 @@ function getJsonObject(path, success, error) {
   xhr.open("GET", path, true);
   xhr.send();
 }
-
+/*
+  Creating the table based on the list provided 
+*/
 function writeTable(list) {
+  
   for (var i = 0; i < list.length; i++) {
-    //console.log(list[i]);
     var tbodyRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
     var newRow = tbodyRef.insertRow();
     var newCheck = newRow.insertCell();
@@ -47,261 +45,227 @@ function writeTable(list) {
     newCategory.innerHTML = list[i].category;
     var img = document.createElement('img');
     img.src = list[i].img;
-    img.classList.add("inverted");
     newImg.appendChild(img);
-    for(var j=0; j<list.rating;j++){
-    var rate = document.createElement('img');
-    rate.src = "images/star-16.ico";
-    rate.classList.add("inverted");
-    newRate.appendChild(rate);
-    //newRate.innerHTML=bookList[i].rating;
+    for (var j = 0; j < list.rating; j++) {
+      var rate = document.createElement('img');
+      rate.src = "images/star-16.ico";
+      newRate.appendChild(rate);
     }
-    for(var j=0; j<list[i].rating;j++){
-    var rate = document.createElement('img');
-    rate.src = "images/star-16.ico";
-    rate.classList.add("inverted");
-    newRate.appendChild(rate);
-    //newRate.innerHTML=bookList[i].rating;
+    for (var j = 0; j < list[i].rating; j++) {
+      var rate = document.createElement('img');
+      rate.src = "images/star-16.ico";
+      newRate.appendChild(rate);
     }
-    var count = 5 - list[i].rating;
-    //console.log(count);
-    for(var k=0; k< count;k++){
+    var countRate = 5 - list[i].rating;
+    for (var k = 0; k < countRate; k++) {
       var unrate = document.createElement('img');
       unrate.src = "images/outline-star-16.ico";
-      unrate.classList.add("inverted");
       newRate.appendChild(unrate);
-      }
-
-    
+    }
   }
-
-
 }
-function search(){
-  
-document.getElementById("btn").addEventListener('click',function(e){
-  //searched = [];
-  bookList.forEach(element => element.searching=false);
+/*
+ Traversing table to check if the given input by user matches any table entry 
+   - If it finds the searching keyword in title of a book it will change its color
+*/
+function searchKey() {
 
+  bookList.forEach(element => element.searching = false);
   const term = document.getElementById("search").value.toLowerCase();
- const titles= []; 
+  const titles = [];
 
- for(var i=1; i< document.getElementById("myTable").rows.length;i++){
-   titles.push(document.getElementById("myTable").rows[i].cells.item(2).innerHTML);
-   
- }
-  if(term ===""){
-    count =0;
-    for(var k = 0;k<document.getElementById("myTable").rows.length;k++){
-      document.getElementById("myTable").rows[k+1].style.backgroundColor = "white";
+  for (var i = 1; i < document.getElementById("myTable").rows.length; i++) {
+    titles.push(document.getElementById("myTable").rows[i].cells.item(2).innerHTML);
+  }
+  if (term === "") {
+    countSearched = 0;
+    for (var k = 0; k < document.getElementById("myTable").rows.length; k++) {
+      if (document.getElementById("darkmode").checked === true) {
+        document.getElementById("myTable").rows[k].style.backgroundColor = "#bbbbbb";
+      }
+      else {
+        document.getElementById("myTable").rows[k].style.backgroundColor = "#FAFCFF";
+      }
     }
-    
-    //console.log("term is blank");
-    return;}
-    count += 1;
-   for(var j=0;j<titles.length;j++){
-    if(titles[j].toLowerCase().indexOf(term)!= -1 && term !== ""){
-      
-      //searched.push(j);
-      
+    return;
+  }
+  countSearched += 1;
+  for (var j = 0; j < titles.length; j++) {
+    if (titles[j].toLowerCase().indexOf(term) != -1 && term !== "") {
       bookList[j].searching = true;
-      document.getElementById("myTable").rows[j+1].style.backgroundColor = "#5898d8";
-      //changeColor(j);
+      currentTable[j].searching = true;
+      if (document.getElementById("darkmode").checked === true) {
+        document.getElementById("myTable").rows[j + 1].style.backgroundColor = "#8e7f7f";
+      }
+      else {
+        document.getElementById("myTable").rows[j + 1].style.backgroundColor = "#5898d8";
+      }
     }
-    
-    else{
-       //console.log("term is:" + term);
-      document.getElementById("myTable").rows[j+1].style.backgroundColor = "white";
+    else {
+      if (document.getElementById("darkmode").checked === true) {
+        document.getElementById("myTable").rows[j + 1].style.backgroundColor = "#bbbbbb";
+      }
+      else {
+        document.getElementById("myTable").rows[j + 1].style.backgroundColor = "#FAFCFF";
+      }
     }
-   }
-  //})
-})}
-function limitcheck(tick){
-  
-    if(tick.checked === false){
-      return;
-     }
-    else{
+  }
+}
+/*
+  Limiting number of checkbox to be selected to one 
+*/
+function limitcheck(tick) {
 
-    for(var i =0; i<document.getElementById("myTable").rows.length-1; i++){
+  if (tick.checked === false) {
+    return;
+  }
+  else {
+
+    for (var i = 0; i < document.getElementById("myTable").rows.length - 1; i++) {
 
       document.getElementById(i).checked = false;
     }
     tick.checked = true;
-    selectedId=tick.id;
-   // console.log(selectedId);
-   }
+    selectedId = tick.id;
+  }
 }
-
-function addCart(){
-
-  var tenure = prompt("Please specify how many of " + document.getElementById("myTable").rows[parseInt(selectedId)+1].cells.item(2).innerHTML + " book you would like to add to your shopping cart?","");
-  countbooks += parseInt(tenure); 
-  document.getElementById(selectedId).checked = false;
-  document.getElementById("qty").innerHTML = "("+countbooks+")";
-}
-function resetCart(){
-  confirm("Is it okay to reset the cart?");
-  countbooks = 0;
-  document.getElementById(selectedId).checked = false;
-  document.getElementById("qty").innerHTML = "(0)";
-}
-function categorize(){
-counter += 1;
-deleteTable();
-bookList.forEach(element => element.categorizing=false);
-console.log(bookList);
-writeTable(bookList);
-currentTable=[];
-if(count > 0){
-  console.log(count);
-  if(document.getElementById("categories").options[document.getElementById("categories").selectedIndex].value === "category"){
-    return;}
-  for(var j=0; j<bookList.length;j++){
-    console.log("you are supposed to be here");
-    console.log(bookList[j].category);
-    console.log(bookList[j].searching);
-    console.log("---------");
-    console.log(document.getElementById("categories").options[document.getElementById("categories").selectedIndex].value);
-    if((bookList[j].searching === true)) {
-      console.log("first criteria met");
-    if(document.getElementById("categories").options[document.getElementById("categories").selectedIndex].value === bookList[j].category.toLowerCase()){
-      console.log("index is"+j);
-      bookList[j].categorizing =true;
-      console.log(bookList[j]);
-      currentTable.push(bookList[j]);
+/*
+  Adding given number of a specific book to the users shopping bag
+ */
+function addCart() {
+  if (selectedId !== "") {
+    var tenure = parseInt(prompt("Please specify how many of " + 
+    document.getElementById("myTable").rows[parseInt(selectedId) + 1].cells.item(2).innerHTML +
+     " book you would like to add to your shopping cart?", ""));
+    if (tenure >= 1) {
+      countBooks += tenure;
+      document.getElementById(selectedId).checked = false;
+      document.getElementById("qty").innerHTML = "(" + countBooks + ")";
+    }
+    else {
+      alert("Please enter a positive number");
+      addCart();
     }
   }
-  console.log(currentTable);
-  
-  
-    
-  //          if(document.getElementById("categories").options[document.getElementById("categories").selectedIndex].value === document.getElementById("myTable").rows[k].cells.item(8).innerHTML.toLowerCase()){
-  //           // bookList[i-1].categorizing = true;
-             
-  //            //categorized.push(document.getElementById("myTable").rows[i].cells.item(2).innerHTML);
-  //  }}
+  else {
+    alert("Please first select a book from the list provided below")
+  }
 }
-deleteTable();
-//console.log(document.getElementById("myTable").innerHTML);
-writeTable(currentTable);
-for(var i=0;i<document.getElementById("myTable").rows.length;i++){
-  document.getElementById("myTable").rows[i].style.backgroundColor = "#5898d8";
-}}
-else{
-if(document.getElementById("categories").options[document.getElementById("categories").selectedIndex].value === "category"){
-  return;
+/*
+  Resetting the cart by setting the book count to zero
+*/
+function resetCart() {
+  if (countBooks === 0) {
+    alert("Your shopping bag is empty");
+  }
+  else {
+    confirm("Is it okay to reset the cart?");
+    countBooks = 0;
+    document.getElementById(selectedId).checked = false;
+    document.getElementById("qty").innerHTML = "(0)";
+  }
 }
-  for(var i =1; i<document.getElementById("myTable").rows.length; i++){
-         if(document.getElementById("categories").options[document.getElementById("categories").selectedIndex].value === document.getElementById("myTable").rows[i].cells.item(8).innerHTML.toLowerCase()){
-          // bookList[i-1].categorizing = true;
-           //console.log(bookList[i-1]);
-           bookList[i-1].categorizing =true;
-           currentTable.push(bookList[i-1]);
-           //categorized.push(document.getElementById("myTable").rows[i].cells.item(2).innerHTML);
- }}
- deleteTable();
- //console.log(document.getElementById("myTable").innerHTML);
- writeTable(currentTable);}
-
- //console.log(currentTable);
-
- //console.log(document.getElementById("myTable").innerHTML);
-
-
-//   if((document.getElementById("categories").options[document.getElementById("categories").selectedIndex].value === "category") ){
-//     //catcount -= 1;
-//     return;
-//   }
-//   else{
-   
-//     console.log("its not category");
-//   categorized = [];
-//   count += 1;
-//   if(count > 1){
-  
-//   writeTable(bookList);
-//   }
-//   console.log("twice");
-//   for(var i =1; i<document.getElementById("myTable").rows.length; i++){
-//     if(document.getElementById("categories").options[document.getElementById("categories").selectedIndex].value !== document.getElementById("myTable").rows[i].cells.item(8).innerHTML.toLowerCase()){
-      
-//           categorized.push(document.getElementById("myTable").rows[i].cells.item(2).innerHTML);
-//     }
-// }
-// for(var j=0;j<categorized.length;j++){
-//   for(var k=1; k< document.getElementById("myTable").rows.length;k++){
-//     if(categorized[j] === document.getElementById("myTable").rows[k].cells.item(2).innerHTML){
-//       //console.warn("inside if statement");
-//       document.getElementById("myTable").deleteRow(k);
-//     }
-//   }
+/*
+  Filtering booklist in accordance with the category selected by the user 
+*/
+function categorize() {
+  countCategorized += 1;
+  deleteTable();
+  bookList.forEach(element => element.categorizing = false);
+  writeTable(bookList);
+  currentTable = [];
+  if (countSearched > 0) {
+    if (document.getElementById("categories")
+    .options[document.getElementById("categories").selectedIndex].value 
+    === "category") {
+      return;
+    }
+    for (var j = 0; j < bookList.length; j++) {
+      if ((bookList[j].searching === true) && 
+      (document.getElementById("categories")
+      .options[document.getElementById("categories").selectedIndex].value 
+      === bookList[j].category.toLowerCase())) {
+          bookList[j].categorizing = true;
+          currentTable.push(bookList[j]);
+      }
+    }
+    deleteTable();
+    writeTable(currentTable);
+    for (var i = 0; i < document.getElementById("myTable").rows.length; i++) {
+      if (document.getElementById("darkmode").checked === true) {
+        document.getElementById("myTable").rows[i].style.backgroundColor = "#8e7f7f";
+      }
+      else {
+        document.getElementById("myTable").rows[i].style.backgroundColor = "#5898d8";
+      }
+    }
+  }
+  else {
+    if (document.getElementById("categories")
+    .options[document.getElementById("categories").selectedIndex].value 
+    === "category") {
+      return;
+    }
+    for (var i = 1; i < document.getElementById("myTable").rows.length; i++) {
+      if (document.getElementById("categories")
+      .options[document.getElementById("categories").selectedIndex].value
+       === document.getElementById("myTable").rows[i].cells.item(8).innerHTML.toLowerCase()) {
+        bookList[i - 1].categorizing = true;
+        currentTable.push(bookList[i - 1]);
+      }
+    }
+    deleteTable();
+    writeTable(currentTable);
+  }
 }
-
-//}}
-function deleteTable(){
-  for(var i = document.getElementById("myTable").rows.length-1;i>0;i--){
+/*
+  Deleting table rows except the heading
+*/
+function deleteTable() {
+  for (var i = document.getElementById("myTable").rows.length - 1; i > 0; i--) {
     document.getElementById("myTable").deleteRow(i);
   }
 }
-function changeColor(id){
+/*
+  Changing the theme on click of the checkbox
+*/
+function changeMode() {
+  document.body.classList.toggle('theme-dark');
+  if (document.getElementById("darkmode").checked === true) {
+    for (var i = 0; i < currentTable.length; i++) {
+      if (currentTable[i].searching === true) {
+        document.getElementById("myTable").rows[i + 1].style.backgroundColor = "#8e7f7f";
 
-
-//   console.log("about to change the color");
-//  // console.log("searched: "+ searched.length);
-//  // console.log("categorized: "+ categorized.length);
-//  // console.log(typeof id);
-//   if((searched.length === 0 && categorized.length !== 0) || (categorized.length === 0 && searched.length !== 0)){
-//document.getElementById("myTable").rows[id+1].style.backgroundColor = "#5898d8";
-
-//     return;
-//   }
-//   else{
-  
-//  // console.log("NONE were empty");
-//   for(var i=0; i<searched.length;i++){
-//     for(var j =0; j<categorized.length;j++){
-//       if(searched[i] === categorized[j]){
-//        // console.log(searched[i] + "-----" + categorized[j]);
-//         document.getElementById("myTable").rows[searched[i] + 1].style.backgroundColor = "blue";
-//       }
-//     }    
-//   }
-// }
-}
-function changeMode(){
-  //console.log("change mode");
-  document.documentElement.classList.toggle('dark-mode');
-  document.querySelectorAll('.inverted').forEach((result) => {
-    result.classList.toggle('invert');
-  })
-
+      }
+      else {
+        document.getElementById("myTable").rows[i + 1].style.backgroundColor = "#bbbbbb";
+      }
+    }
+  }
+  else {
+    console.log("we are here");
+    for (var i = 0; i < currentTable.length; i++) {
+      console.log(currentTable[i]);
+      if (currentTable[i].searching === true) {
+        console.log("search is true");
+        document.getElementById("myTable").rows[i + 1].style.backgroundColor = "#5898d8";
+      }
+      else {
+        document.getElementById("myTable").rows[i + 1].style.backgroundColor = "#FAFCFF";
+      }
+    }
+  }
 }
 window.onload = function () {
-   // book list container
   getJsonObject('data.json',
     function (data) {
-      bookList = data; // store the book list into bookList
-      for(var i=0; i< bookList.length;i++){
+      bookList = data; 
+      currentTable = bookList;
+      for (var i = 0; i < bookList.length; i++) {
         bookList[i].searching = false;
         bookList[i].categorizing = false;
       }
-       //console.log(bookList); // print it into console (developer tools)
-      // console.log(bookList[0].authors); // print the first book object into console 
-      // here you can call methods to laod or refresh the page 
-      // loadBooks() or refreshPage()
-     // currentTable = bookList;
-      // for(var i=0;i<currentTable.length;i++){
-      //   console.log(currentTable[i]);
-      // }
-     // console.log("onload");
       writeTable(bookList);
-      //console.log(document.getElementById("myTable").rows[0].innerHTML);
-      search();
-      
-      //limitcheck();
-
-
     },
     function (xhr) {
       console.error(xhr);
